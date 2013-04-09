ed323b59fd062b2f4e2de34cac254b39fd1ac9fe
Jeremy Carbaugh <jcarbaugh@gmail.com>
2013-04-09 19:01:50 -0400

    another live attempt at showing the caller

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 from os import environ
 
-from flask import Flask, Response
+from flask import Flask, Response, request
 from twilio.rest import TwilioRestClient
 
 ACCOUNT_SID = environ.get('TWILIO_ACCOUNT_SID')
@@ -12,22 +12,25 @@ HOST_NUMBER = environ.get('HOST_NUMBER')
 app = Flask(__name__)
 
 
-# def format_phone(s):
-#     if not s.startswith('+'):
-#         return s
-#     s = s[1:]
-#     return "%s-%s-%s" % (s[1:4], s[4:7], s[7:])
+def format_phone(s):
+    if not s:
+        return 'unknown'
+    if not s.startswith('+'):
+        return s
+    s = s[1:]
+    return "%s-%s-%s" % (s[1:4], s[4:7], s[7:])
 
 
 @app.route('/')
 def index():
+
+    caller = format_phone(request.args.get('Caller'))
+    message = "There is someone at the door: %s" % caller
+
     client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
-    # client.sms.messages.create(to="+%s" % HOST_NUMBER,
-    #                            from_="+%s" % TWILIO_NUMBER,
-    #                            body="There is someone at the door: %s" % format_phone(request.args.get('From')))
     client.sms.messages.create(to="+%s" % HOST_NUMBER,
                                from_="+%s" % TWILIO_NUMBER,
-                               body="There is someone at the door")
+                               body=message)
     return Response("""
             <Response>
                 <Say>Welcome to Jango District. Someone will be with you in a moment to let you in.</Say>
